ec61d62109260e74205cafade4b471d645a0a1a2
v4xiaoyu <v4xiaoyu@gmail.com>
2017-09-04 16:56:04 +0800

    init learning

--- src/client/client.go
+++ src/client/client.go
@@ -3,6 +3,9 @@ package main
 import (
 	"../utils"
 	"fmt"
+	"github.com/sjwhitworth/golearn/base"
+	"github.com/sjwhitworth/golearn/evaluation"
+	"github.com/sjwhitworth/golearn/knn"
 	"time"
 )
 
@@ -42,6 +45,40 @@ func client() {
 	}
 }
 
+func learn() {
+	// Load in a dataset, with headers. Header attributes will be stored.
+	// Think of instances as a Data Frame structure in R or Pandas.
+	// You can also create instances from scratch.
+	rawData, err := base.ParseCSVToInstances("../../datasets/house-votes-84.csv", true)
+	if err != nil {
+		panic(err)
+	}
+
+	// Print a pleasant summary of your data.
+	fmt.Println(rawData)
+
+	//Initialises a new KNN classifier
+	cls := knn.NewKnnClassifier("euclidean", "linear", 2)
+
+	//Do a training-test split
+	trainData, testData := base.InstancesTrainTestSplit(rawData, 0.50)
+	cls.Fit(trainData)
+
+	//Calculates the Euclidean distance and returns the most popular label
+	predictions, err := cls.Predict(testData)
+	if err != nil {
+		panic(err)
+	}
+
+	// Prints precision/recall metrics
+	confusionMat, err := evaluation.GetConfusionMatrix(testData, predictions)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
+	}
+	fmt.Println(evaluation.GetSummary(confusionMat))
+}
+
 func main() {
-	client()
+	//client()
+	learn()
 }
