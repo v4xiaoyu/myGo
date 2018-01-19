a272caa4a128fe17aaa5574cf298eb6ecd87165c
v4xiaoyu <v4xiaoyu@gmail.com>
2018-01-19 10:21:44 +0800

    init block chain

---
+++ src/blockchain/blockchain.go
@@ -0,0 +1,31 @@
+package blockchain
+
+import (
+	"fmt"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+type SampleChaincode struct {
+}
+
+func (t *SampleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	return nil, nil
+}
+
+func (t *SampleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	return nil, nil
+}
+
+func (t *SampleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	return nil, nil
+}
+
+func main() {
+	err := shim.Start(new(SampleChaincode))
+	if err != nil {
+		fmt.Println("Could not start SampleChaincode")
+	} else {
+		fmt.Println("SampleChaincode successfully started")
+	}
+
+}
