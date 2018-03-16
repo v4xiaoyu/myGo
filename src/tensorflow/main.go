84d1fe6ee0ff758160968c2bd8820b17309d36b3
v4xiaoyu <v4xiaoyu@gmail.com>
2018-03-16 14:30:19 +0800

    init tensor flow

---
+++ src/tensorflow/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	_ "fmt"
+	tf "github.com/tensorflow/tensorflow/tensorflow/go"
+	op "github.com/tensorflow/tensorflow/tensorflow/go/op"
+)
+
+func main() {
+	s := op.NewScope()
+	c := op.Const(s, "Hello from TensorFlow version "+tf.Version())
+	graph, err := s.Finalize()
+	if err != nil {
+		panic(err)
+	}
+
+	// Execute the graph in a session.
+	sess, err := tf.NewSession(graph, nil)
+	if err != nil {
+		panic(err)
+	}
+	output, err := sess.Run(nil, []tf.Output{c}, nil)
+	if err != nil {
+		panic(err)
+	}
+	println(output[0].Value())
+}
